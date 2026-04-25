ba79c7d1ea20c8cc2b0eb380cd200d2a047810fa
ガンマ技術部 <yu1000.scratch@gmail.com>
2026-04-25 20:08:03 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ import { renderMemos } from "./ui.js";
 let memos = loadMemos();
 renderMemos(memos);
 
+// 1秒ごとに残り時間更新
+setInterval(() => {
+    memos = loadMemos();
+    renderMemos(memos);
+}, 1000);
+
 document.getElementById("newMemoBtn").addEventListener("click", () => {
     window.location.href = "edit.html";
 });
